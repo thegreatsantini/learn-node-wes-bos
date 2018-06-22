f8596deab997c8e2bffccf2695b23f1f67a7318a
thegreatsantini <lss13@my.fsu.edu>
2018-06-21 22:49:17 -0700

    completed vid 11, async await...very interesting :)

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); // look at Start.js uses Singleton
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
@@ -6,6 +9,16 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' })
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+
+
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    Store.find({ name: req.body.name }, function(err,success){
+        if (success) {
+            console.log(success)
+        }
+    })
+    res.redirect('/');
+    
 }
