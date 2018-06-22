dc9e38168f809783f2af3164b096bc3e3c01c28b
thegreatsantini <lss13@my.fsu.edu>
2018-06-21 22:14:18 -0700

    vid 10

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse)
-})
 
 module.exports = router;
